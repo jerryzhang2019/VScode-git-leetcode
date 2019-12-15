a2f74000b3b979bfd4584a2d97f138209e562142
engineerjerry1@gmail.com <engineerjerry1@gmail.com>
2019-12-14 16:37:57 -0800

    update my code

--- Leetcode高频考题重中之重/leetcoede斐波那契额数列/509.FibonacciNumber.py
+++ Leetcode高频考题重中之重/leetcoede斐波那契额数列/509.FibonacciNumber.py
@@ -1,6 +1,8 @@
+'''
 # The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is
 # the sum of the two preceding ones, starting from 0 and 1. That is, F(0) = 0,   F(1) = 1
 # F(N) = F(N - 1) + F(N - 2), for N > 1. Given N, calculate F(N)。
+'''
 # 问题1： 打印斐波那契的指定的莫一个数
 class Solution:
     def fib(self, N:int) -> int:
@@ -12,7 +14,9 @@ if __name__ == '__main__':
     N = 5
     result = Solution().fib(N)  # 只打印某一个数
     print(result)
+
 # 问题2：打印斐波那契数的前N个数？比如打印前10个数
+
 class Fib(object):
     def __init__(self, num):
         a, b, L = 0, 1, []
