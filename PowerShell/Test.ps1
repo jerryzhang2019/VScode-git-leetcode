b3419e62776d17630bb4c884aea153f406b1f869
Qi Zhang (NORTHWEST CONTRACT SERVICES IN) <v-qizha9@microsoft.com>
2019-12-27 16:20:20 -0800

    update my study code

---
+++ PowerShell/Test.ps1
@@ -0,0 +1,25 @@
+# 数学运算
+1+1
+2-1
+1*2
+1/2
+# 打印字符串
+"Hello World"
+# 四则混合运算
+(5+23*2)/23
+#运算符
+1,2,3,5,3,2 -contains 3  # 此数组中是否包含3
+1,2,3,5,3,2 -eq 3 # 返回所有等于3的元素
+1,2,3,5,3,2 -lt 3 # 返回所有小于3的元素
+# if(1,3,5 -contains2): # 测试2是否在集合中
+# 类型转换
+#(2+2)*3/7 > c:/foo.txt
+# 存储到临时变量
+$n = (2+2)*3
+$n
+$n/7
+# 运算结果存储到文件
+# $files = dir
+# $files[ 3]
+# 在PowerShell中, 命令分为四类: cmdlet, function, script和native Windows commands.
+#get-command -CommandType
---
+++ PowerShell/foreach.ps1
@@ -0,0 +1,7 @@
+$var = 1,2,3,4,5,6
+foreach($i in $var)
+{
+    $n++
+    Write-Host"$i"
+}
+Write-Host"there were $n record"
