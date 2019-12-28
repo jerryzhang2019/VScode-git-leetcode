af21c0a2e983ba189a1abc3d6ee78172fdab1f9a
Qi Zhang (NORTHWEST CONTRACT SERVICES IN) <v-qizha9@microsoft.com>
2019-12-27 16:32:18 -0800

    update my code

--- PowerShell/Test.ps1
+++ PowerShell/Test.ps1
@@ -23,3 +23,11 @@ $n/7
 # $files[ 3]
 # 在PowerShell中, 命令分为四类: cmdlet, function, script和native Windows commands.
 #get-command -CommandType
+#调用运算符
+#函数的用法
+$var1=10
+function one{"The variable is $var1"}
+function two{$var1=20;one}
+one # The variable is 10
+two # The variable is 20
+one # The variable is 10
