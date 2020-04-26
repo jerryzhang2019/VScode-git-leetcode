797991dcfc06460db917634c0d16a7b2036c1e86
jerryzhang2019 <engineerjerry1@gmail.com>
2020-04-26 15:46:38 -0700

    update my excise to server

--- Web_FrontEnd/Javascript/冰激凌.js
+++ Web_FrontEnd/Javascript/冰激凌.js
@@ -39,8 +39,11 @@
  */
  
 // change the values of `flavor`, `vessel`, and `toppings` to test your code
-var flavor = "strawberry";
+var flavor = "vanilla";
 var vessel = "cone";
-var toppings = "cookies";
+var toppings = "peanuts";
 
 // Add your code here
+if((flavor==="vanilla"||flavor==="chocolate")&&(vessel==="cone"||vessel==="bowl")&&(toppings==="sprinkles"||toppings==="peanuts")){
+    console.log("I'd like two scoops of"+flavor+"ice cream in a"+ vessel+"with"+toppings+".")
+}
