7f2a638aa5fb450ba38e961ff697fd8eedf61217
jerryzhang2019 <engineerjerry1@gmail.com>
2020-05-02 10:24:44 -0700

    update my code to server

---
+++ Web_FrontEnd/Javascript/数组/价格正确.js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `prices`
+ * - The variable `prices` should be an array having different values for the 1st, 3rd, and 7th elements
+ * - Your code should print `prices` to the console as an array. Do not iterate over the elements. 
+ */
+ 
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+// your code goes here
+prices[0]=1;
+prices[2]=3;
+prices[6]=7;
+console.log(prices);
