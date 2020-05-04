361186760cc1f3846857f492534026b9a854597b
jerryzhang2019 <engineerjerry1@gmail.com>
2020-05-03 19:31:09 -0700

    update my code to server

---
+++ Web_FrontEnd/Javascript/对象/菜单.js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: Menu Items (7-2)
+ * Create an object named `breakfast`. 
+ * The object should contain properties for the `name`, `price`, and `ingredients`.
+ * Print the entire object on the console
+ */
+
+// your code goes here
+var breakfast={
+    name:"The Lumberjack",
+    price:"$9.95",
+    ingredients:["eggs", "sausage", "toast", "hashbrowns", "pancakes"]
+};
+
+breakfast.name();
+breakfast.price();
+breakfast.ingredients();
