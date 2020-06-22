209c69954bbce4d53d27415337123f03495227c6
jerryzhang2019 <engineerjerry1@gmail.com>
2020-06-21 21:37:11 -0700

    upload my local code to server

--- Web_FrontEnd/Project4_Evaluate-news-nlp/evaluate-news-nlp/src/server/index.js
+++ Web_FrontEnd/Project4_Evaluate-news-nlp/evaluate-news-nlp/src/server/index.js
@@ -2,7 +2,20 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+var aylien = require('aylien_textapi');
+const bodyParser = require('body-parser');
+
+require('dotenv').config()
+
+const textapi = new aylien({
+    appilication_id: `${process.env.API_ID}`,
+    appilication_key:`${process.env.API_KEY}`
+});
+
 const app = express()
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.use(express.static('dist'))
 
@@ -16,6 +29,23 @@ app.get('/', function (req, res) {
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
+})
+
+app.post('/testing',async(req, res, next) =>{
+    console.log(req.body);
+    try{
+        var data=textapi.sentiment({
+            'text':req.body.theText
+        },function(error,response){
+            if(error === null){
+                console.log(response);
+                res.send(response);
+            }
+        });
+
+    }catch(error){
+        return next(error)
+    }
 })
 
 app.get('/test', function (req, res) {
--- Web_FrontEnd/Project4_Evaluate-news-nlp/evaluate-news-nlp/webpack.prod.js
+++ Web_FrontEnd/Project4_Evaluate-news-nlp/evaluate-news-nlp/webpack.prod.js
@@ -1,16 +1,27 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extral-plugin');
+const OptimizeCSSAssetPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const workboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    optimization:{
+      minimizer:[new TerserPlugin({}), new OptimizeCSSAssetPlugin({})],  
+    },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test:/\.js$/,
+                use:[MiniCssExtractPlugin.loader, 'css-loader','sass-loader']
             }
         ]
     },
@@ -18,6 +29,11 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({filename:'[name].css'}),
+        new workboxPlugin.GenerateSW({
+            clientsClaim:true,
+            skipWaiting:true
+        }),
     ]
 }
